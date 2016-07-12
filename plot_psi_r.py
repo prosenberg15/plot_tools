552b81cc436cec3ef5e9282b44ff03360584f0b7
Peter Rosenberg <prosenberg15@gmail.com>
2016-07-12 17:37:37 -0400

    current version

--- plot_psi_r.py
+++ plot_psi_r.py
@@ -19,13 +19,15 @@ from plot_tools import *
 rc('text', usetex=True)
 rc('font', family='serif')
 
-Nl=np.array([12,12])
-Np=144
+Nl=np.array([8,8])
+Np=64
 Ntot=Nl[0]*Nl[1]
+Uhubb=-2.5
+lbda=1.5
+dim_flag='2D'
 
 shift=np.zeros(Ntot)
 shift.fill(1)
-
 
 if ('didj' in sys.argv[1]):
     plot_flag='didj'
@@ -49,9 +51,21 @@ if('ninj' in plot_flag):
     # nu*nu
     nui_nuj=np.array(data[:Nl[0]*Nl[1],1])
     nui_nuj_err=np.array(data[:Nl[0]*Nl[1],3])
+
+    for arg in sys.argv:
+        if('open' in arg):
+            open_flg='open_BCs'
+            #nui_ndj=nui_ndj/np.sqrt(nui_nuj[0])
+            #nui_nuj=nui_nuj/np.sqrt(nui_nuj[0])
+
     # nu*nd + nu*nu
     ninj=nui_ndj+nui_nuj
     ninjerr=nui_ndj_err+nui_nuj_err
+
+    # scale down self-interaction peak at origin
+    nui_ndj[0]=nui_ndj[0]/2.0
+    nui_nuj[0]=nui_nuj[0]/2.0
+    ninj[0]=ninj[0]/2.0
 
     lattlabel=np.loadtxt(sys.argv[2+arg_shift])
     # shift, so lattice site 1 has coords (x,y)=(0,0) (not (1,1))
@@ -62,10 +76,12 @@ if('ninj' in plot_flag):
     
     # shift x,y coords (currently stored as lists)
     ninj_x=shift_origin_list(ninj_x,Nl[0])
-    ninj_y=shift_origin_list(ninj_y,Nl[0])
+    ninj_y=shift_origin_list(ninj_y,Nl[1])
 
     xi, yi = np.ogrid[ninj_x.min():ninj_x.max():200j, ninj_y.min():ninj_y.max():200j]
-
+    # ogrid gives transposed axes, so transpose back
+    xi, yi = xi.T, yi.T
+    
     corr_flag = str(raw_input("nu*nu, nu*nd, ntot*ntot? : "))
     if('nu*nu' in corr_flag):
         # nu*nu
@@ -94,12 +110,18 @@ if('ninj' in plot_flag):
         plot_func=ninj_2D_i
 
 elif('didj' in plot_flag):
+
+    for arg in sys.argv:
+        if('open' in arg):
+            open_flg='open_BCs'
+
     #load didj data
     data=np.loadtxt(sys.argv[1+arg_shift],dtype=np.float64)
     didj=data[:,1]
     didjerr=data[:,3]
     # scale origin
-    #didj[0]=0.01#didj[0]/10.0
+    #didj[0]=0.01
+    didj[0]=didj[0]/10.0
     # normalize
     #didj=didj[:]*(Nl[0]*Nl[1])**2/(Np*(Np-1))
     #didjerr=didjerr[:]*(Nl[0]*Nl[1])**2/(Np*(Np-1))
@@ -113,9 +135,11 @@ elif('didj' in plot_flag):
     
     # shift x,y coords (currently stored as lists)
     didj_x=shift_origin_list(didj_x,Nl[0])
-    didj_y=shift_origin_list(didj_y,Nl[0])
+    didj_y=shift_origin_list(didj_y,Nl[1])
 
     xi, yi = np.ogrid[didj_x.min():didj_x.max():200j, didj_y.min():didj_y.max():200j]
+    # ogrid gives transposed axes, so transpose back
+    xi, yi = xi.T, yi.T
 
     # reshape list --> 2D array
     didj_2D=reshape_1D_to_2D(didj_x,didj_y,didj)
@@ -154,13 +178,14 @@ elif('psi_r' in plot_flag):
     psi_r_s=psidata[2*Ntot:,0]+1j*psidata[2*Ntot:,1]
     # |psi_s|
     mod_psi_r_s=np.sqrt((psi_r_s[:].real)**2+(psi_r_s[:].imag)**2)
-    mod_psi_r_s_re=psi_r_s[:].real
-    mod_psi_r_s_im=psi_r_s[:].imag
+    sing_re=psi_r_s[:].real
+    sing_im=psi_r_s[:].imag
     ##############################################################
     # |psi_tot|
     mod_psi_r_tot=mod_psi_r_s+mod_dd+mod_psi_r
-    mod_psi_r_tot_re=psi_r_s[:].real+dd[:].real+psi_r[:].real
-    mod_psi_r_tot_im=psi_r_s[:].imag+dd[:].imag+psi_r[:].imag
+    # ERROR HERE (|psi_tot| =\= sqrt(Re(psi_s)+Re(psi_uu)+Re(psi_dd))^2+Im(psi_s)+Im(psi_uu)+Im(psi_dd))^2)
+    tot_re=psi_r_s[:].real+dd[:].real+psi_r[:].real
+    tot_im=psi_r_s[:].imag+dd[:].imag+psi_r[:].imag
 
     # shift x,y coords (currently stored as lists), so origin is at center, not edge of plot 
     psi_x=shift_origin_list(psi_x,Nl[0])
@@ -186,9 +211,13 @@ elif('psi_r' in plot_flag):
     """
 
     xi, yi = np.ogrid[psi_x.min():psi_x.max():200j, psi_y.min():psi_y.max():200j]
+    # ogrid gives transposed axes, so transpose back
+    xi, yi = xi.T, yi.T
+
     #xi_trunc, yi_trunc = np.ogrid[psi_x_trunc.min():psi_x_trunc.max():200j, psi_y_trunc.min():psi_y_trunc.max():200j]
 
     if('dd' in sys.argv[1]):
+        plot_flag=plot_flag+'_dd'
         # modulus wfn
         # reshape list --> 2D array
         dd_2D=reshape_1D_to_2D(psi_x,psi_y,mod_dd)
@@ -236,6 +265,7 @@ elif('psi_r' in plot_flag):
         """
 
     elif('sing' in sys.argv[1] or '_s' in sys.argv[1]):
+        plot_flag=plot_flag+'_s'
         # modulus wfn
         # reshape list --> 2D array
         mod_psi_r_s_2D=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_s)
@@ -248,23 +278,24 @@ elif('psi_r' in plot_flag):
 
         # real part
         # reshape list --> 2D array
-        mod_psi_r_s_2D_re=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_s_re)
+        sing_2D_re=reshape_1D_to_2D(psi_x,psi_y,sing_re)
         # shift origin
-        mod_psi_r_s_2D_re=shift_origin_array(mod_psi_r_s_2D_re,Nl)
+        sing_2D_re=shift_origin_array(sing_2D_re,Nl)
         # interpolate
-        mod_psi_r_s_2D_re_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_s_2D_re,3,3,0)
-        plot_func_re=mod_psi_r_s_2D_re_i
+        sing_2D_re_i=splineinterp_2D(psi_x,psi_y,sing_2D_re,3,3,0)
+        plot_func_re=sing_2D_re_i
 
         # imag part
         # reshape list --> 2D array
-        mod_psi_r_s_2D_im=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_s_im)
+        sing_2D_im=reshape_1D_to_2D(psi_x,psi_y,sing_im)
         # shift origin
-        mod_psi_r_s_2D_im=shift_origin_array(mod_psi_r_s_2D_im,Nl)
+        sing_2D_im=shift_origin_array(sing_2D_im,Nl)
         # interpolate
-        mod_psi_r_s_2D_im_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_s_2D_im,3,3,0)
-        plot_func_im=mod_psi_r_s_2D_im_i
+        sing_2D_im_i=splineinterp_2D(psi_x,psi_y,sing_2D_im,3,3,0)
+        plot_func_im=sing_2D_im_i
 
     elif('tot' in sys.argv[1]):
+        plot_flag=plot_flag+'_tot'
         # modulus wfn.
         # reshape list --> 2D array
         mod_psi_r_tot_2D=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_tot)
@@ -276,26 +307,27 @@ elif('psi_r' in plot_flag):
 
         # real part
         # reshape list --> 2D array
-        mod_psi_r_tot_2D_re=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_tot_re)
+        tot_2D_re=reshape_1D_to_2D(psi_x,psi_y,tot_re)
         # shift origin
-        mod_psi_r_tot_2D_re=shift_origin_array(mod_psi_r_tot_2D_re,Nl)
+        tot_2D_re=shift_origin_array(tot_2D_re,Nl)
         # interpolate
-        mod_psi_r_tot_2D_re_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_tot_2D_re,3,3,0)
-        plot_func_re=mod_psi_r_tot_2D_re_i
+        tot_2D_re_i=splineinterp_2D(psi_x,psi_y,tot_2D_re,3,3,0)
+        plot_func_re=tot_2D_re_i
 
         # imag part
         # reshape list --> 2D array
-        mod_psi_r_tot_2D_im=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_tot_im)
+        tot_2D_im=reshape_1D_to_2D(psi_x,psi_y,tot_im)
         # shift origin
-        mod_psi_r_tot_2D_im=shift_origin_array(mod_psi_r_tot_2D_im,Nl)
+        tot_2D_im=shift_origin_array(tot_2D_im,Nl)
         # interpolate
-        mod_psi_r_tot_2D_im_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_tot_2D_im,3,3,0)
-        plot_func_im=mod_psi_r_tot_2D_im_i
+        tot_2D_im_i=splineinterp_2D(psi_x,psi_y,tot_2D_im,3,3,0)
+        plot_func_im=tot_2D_im_i
 
     else:
         print 'must choose part of wfn to plot'
         wfnflag = str(raw_input("dd, sing, tot? : "))
         if('dd' in wfnflag):
+            plot_flag=plot_flag+'_dd'
             # mod wfn
             mod_psi_r_dd_2D=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_dd)
             mod_psi_r_dd_2D=shift_origin_array(mod_psi_r_dd_2D,Nl)
@@ -315,139 +347,172 @@ elif('psi_r' in plot_flag):
             plot_func_im=mod_psi_r_dd_2D_im_i
 
         elif('sing' in wfnflag):
+            plot_flag=plot_flag+'_s'
             # mod wfn.
             mod_psi_r_s_2D=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_s)
             mod_psi_r_s_2D=shift_origin_array(mod_psi_r_s_2D,Nl)
             mod_psi_r_s_2D_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_s_2D,3,3,0)
 
             # real part
-            mod_psi_r_s_2D_re=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_s_re)
-            mod_psi_r_s_2D_re=shift_origin_array(mod_psi_r_s_2D_re,Nl)
-            mod_psi_r_s_2D_re_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_s_2D_re,3,3,0)
+            sing_2D_re=reshape_1D_to_2D(psi_x,psi_y,sing_re)
+            sing_2D_re=shift_origin_array(sing_2D_re,Nl)
+            sing_2D_re_i=splineinterp_2D(psi_x,psi_y,sing_2D_re,3,3,0)
 
             # imag part
-            mod_psi_r_s_2D_im=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_s_im)
-            mod_psi_r_s_2D_im=shift_origin_array(mod_psi_r_s_2D_im,Nl)
-            mod_psi_r_s_2D_im_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_s_2D_im,3,3,0)
+            sing_2D_im=reshape_1D_to_2D(psi_x,psi_y,sing_im)
+            sing_2D_im=shift_origin_array(sing_2D_im,Nl)
+            sing_2D_im_i=splineinterp_2D(psi_x,psi_y,sing_2D_im,3,3,0)
 
             plot_func=mod_psi_r_s_2D_i
-            plot_func_re=mod_psi_r_s_2D_re_i
-            plot_func_im=mod_psi_r_s_2D_im_i
+            plot_func_re=sing_2D_re_i
+            plot_func_im=sing_2D_im_i
 
         elif('tot' in wfnflag):
+            plot_flag=plot_flag+'_tot'
             # mod wfn.
             mod_psi_r_tot_2D=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_tot)
             mod_psi_r_tot_2D=shift_origin_array(mod_psi_r_tot_2D,Nl)
             mod_psi_r_tot_2D_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_tot_2D,3,3,0)
 
             # real part
-            mod_psi_r_tot_2D_re=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_tot_re)
-            mod_psi_r_tot_2D_re=shift_origin_array(mod_psi_r_tot_2D_re,Nl)
-            mod_psi_r_tot_2D_re_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_tot_2D_re,3,3,0)
+            tot_2D_re=reshape_1D_to_2D(psi_x,psi_y,tot_re)
+            tot_2D_re=shift_origin_array(tot_2D_re,Nl)
+            tot_2D_re_i=splineinterp_2D(psi_x,psi_y,tot_2D_re,3,3,0)
 
             # imag part
-            mod_psi_r_tot_2D_im=reshape_1D_to_2D(psi_x,psi_y,mod_psi_r_tot_im)
-            mod_psi_r_tot_2D_im=shift_origin_array(mod_psi_r_tot_2D_im,Nl)
-            mod_psi_r_tot_2D_im_i=splineinterp_2D(psi_x,psi_y,mod_psi_r_tot_2D_im,3,3,0)
+            tot_2D_im=reshape_1D_to_2D(psi_x,psi_y,tot_im)
+            tot_2D_im=shift_origin_array(tot_2D_im,Nl)
+            tot_2D_im_i=splineinterp_2D(psi_x,psi_y,tot_2D_im,3,3,0)
 
             plot_func=mod_psi_r_tot_2D_i
-            plot_func_re=mod_psi_r_tot_2D_re_i
-            plot_func_im=mod_psi_r_tot_2D_im_i
+            plot_func_re=tot_2D_re_i
+            plot_func_im=tot_2D_im_i
 
 #print mod_psi_r_dd.max()
 # grab 1D slice
 # slice defined by starting point (x0,y0)
 # and direction vector (xsdir,ysdir)
+"""
 x0=psi_x.min()
-y0=1
+y0=0
 xsdir=1
 ysdir=0
 
-#xsexact,ysexact,qsexact,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,nui_nuj,[0]*len(ninj))
+#xsexact,ysexact,qsexact,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,ninj,[0]*len(ninj))
 
-xsexact,ysexact,qsexact,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,mod_dd,[0]*len(dd))
-#print "xs :", xsexact
-#print "ys :", ysexact
-xsexact,ysexact,qsexact_re,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,dd_re,[0]*len(dd_re))
-#print "xs :", xsexact
-#print "ys :", ysexact
-xsexact,ysexact,qsexact_im,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,dd_im,[0]*len(dd_im))
-#print "sqrt(qs_re^2+qs_im^2) :", np.sqrt(np.array(qsexact_im)**2+np.array(qsexact_re)**2)
+xsexact,ysexact,qsexact,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,mod_psi_r_tot,[0]*len(mod_psi_r_tot))
+xsexact,ysexact,qsexact_re,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,tot_re,[0]*len(tot_re))
+#xsexact,ysexact,qsexact_im,qsexacterr=grab_slice(x0,y0,xsdir,ysdir,psi_x,psi_y,tot_im,[0]*len(tot_im))
+
 xsexact=np.array(xsexact)
 ysexact=np.array(ysexact)
-
-#print qsexact
+"""
 # set up figure environment
 fig = plt.figure()
 
-#for 1D plot uncomment below
+#for 1D plot
 ###############################
+if '1D' in dim_flag:
+    ax=fig.add_subplot(111)
+    ax.set_xticks(np.arange(xi.min(),xi.max()+1,1))
 
-ax=fig.add_subplot(111)
-ax.set_xticks(np.arange(xi.min(),xi.max()+1,1))
+    xnew=np.linspace(xsexact.min(),xsexact.max(),100)
+    #ynew=np.linspace(ysexact.min(),ysexact.max(),100)
+    #xnewfunc=xnew/(1.0*Nl[0])
+    ynew=np.empty(len(xnew))
+    ynew.fill(y0)
+    #ynewfunc=ynew/(1.0*Nl[1])
+    #ax.plot(xnew*np.sqrt(2.0),plot_func_trunc.ev(xnewfunc,ynewfunc))
 
-xnew=np.linspace(xsexact.min(),xsexact.max(),100)
-#ynew=np.linspace(ysexact.min(),ysexact.max(),100)
-#xnewfunc=xnew/(1.0*Nl[0])
-ynew=np.empty(len(xnew))
-ynew.fill(y0)
-#ynewfunc=ynew/(1.0*Nl[1])
-#ax.plot(xnew*np.sqrt(2.0),plot_func_trunc.ev(xnewfunc,ynewfunc))
-ax.plot(xnew,plot_func.ev(ynew,xnew),color='orange')
-ax.plot(xnew,plot_func_re.ev(ynew,xnew),color='blue')
-ax.plot(xnew,plot_func_im.ev(ynew,xnew),color='green')
-ax.plot(xnew,np.sqrt(plot_func_re.ev(ynew,xnew)**2+plot_func_im.ev(ynew,xnew)**2),color='purple')
+    # caution : RectBivariateSpline flips x & y axes, so must enter arguments as (y,x), not (x,y)
+    #ax.plot(xnew,plot_func.ev(ynew,xnew),color='orange')
+    ax.plot(xnew,plot_func_re.ev(ynew,xnew),color='blue')
+    #ax.plot(xnew,plot_func_im.ev(ynew,xnew),color='green')
+    #ax.plot(xnew,np.sqrt(plot_func_re.ev(ynew,xnew)**2+plot_func_im.ev(ynew,xnew)**2),color='purple')
 
-#xsexact=np.sqrt(2.0)*xsexact
-ax.plot(xsexact,qsexact,linestyle='None',color='red',marker='o')
-ax.plot(xsexact,qsexact_re,linestyle='None',color='black',marker='s')
-ax.plot(xsexact,qsexact_im,linestyle='None',color='cyan',marker='^')
+    #xsexact=np.sqrt(2.0)*xsexact
+    #ax.plot(xsexact,qsexact,linestyle='None',color='red',marker='o')
+    ax.plot(xsexact,qsexact_re,linestyle='None',color='black',marker='s')
+    #ax.plot(xsexact,qsexact_im,linestyle='None',color='cyan',marker='^')
 
-#for 2D plot uncomment below
+#for 2D plot
 ###############################
-"""
-ax=fig.add_subplot(111)
-ax.set_xticks(np.arange(xi.min(),xi.max()+1,1))
-ax.set_yticks(np.arange(yi.min(),yi.max()+1,1))
-ax.set_xlabel('x',fontsize=24)
-ax.set_ylabel('y',fontsize=24,rotation=360)
-ax.xaxis.set_tick_params(labelsize=22)
-ax.yaxis.set_tick_params(labelsize=22)
+elif '2D' in dim_flag:
+    ax=fig.add_subplot(111)
+    ax.set_xticks(np.arange(xi.min(),xi.max()+1,1))
+    ax.set_yticks(np.arange(yi.min(),yi.max()+1,1))
+    ax.set_xlabel('x',fontsize=24)
+    ax.set_ylabel('y',fontsize=24,rotation=360)
+    ax.xaxis.set_tick_params(labelsize=22)
+    ax.yaxis.set_tick_params(labelsize=22)
 
-implot = ax.imshow(plot_func(xi,yi),extent=(xi.min(),xi.max(),yi.min(),yi.max()),cmap=cm.gnuplot2,origin='lower')
+    # caution : RectBivariateSpline flips x & y axes, so must enter arguments as (y,x), not (x,y)
+    if (('psi_r' not in plot_flag) or ('tot' in plot_flag)):
+        implot = ax.imshow(plot_func(yi,xi),extent=(xi.min(),xi.max(),yi.min(),yi.max()),cmap=cm.gnuplot2,origin='lower')
+    else:
+        implot = ax.imshow(np.sqrt(plot_func_re(yi,xi)**2+plot_func_im(yi,xi)**2),extent=(xi.min(),xi.max(),yi.min(),yi.max()),cmap=cm.gnuplot2,origin='lower')
 
-cb=fig.colorbar(implot,shrink=1.0,pad=0.01)
-cb.ax.tick_params(labelsize=22)
-"""
-#for 3D plot uncomment below
+    cb=fig.colorbar(implot,shrink=1.0,pad=0.01)
+    cb.ax.tick_params(labelsize=22)
+
+#for 3D plot
 ###############################
-"""
-ax=fig.gca(projection = '3d')
+elif '3D' in dim_flag:
+    ax=fig.gca(projection = '3d')
 
-# fix aspect ratio if lattice isn't square
-# otherwise use ratio of 1.0
-if(abs(Nl[0]-Nl[1])>1e-9):
-    ax.pbaspect = [0.75, 1.5,0.5]
-elif(abs(Nl[0]-Nl[1])<1e-9):
-    ax.pbaspect = [1.0,1.0,0.5]
+    # fix aspect ratio if lattice isn't square
+    # otherwise use ratio of 1.0
 
-# axis labels and limits
-ax.set_xlim(-Nl[0]/2,Nl[0]/2-1)
-ax.set_ylim(-Nl[1]/2,Nl[1]/2-1)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
+    if(abs(Nl[0]-Nl[1])>1e-9):
+        ax.pbaspect = [0.75, 1.5,0.5]
+    elif(abs(Nl[0]-Nl[1])<1e-9):
+        ax.pbaspect = [1.0,1.0,1.0]
+        #ax.pbaspect = [1.0,1.0,0.85]
 
-ax.scatter(psi_x, psi_y, dd_re+0.001, s=20, c='red')
-surf = ax.plot_surface(xi, yi, plot_func_re(xi,yi), rstride=1, cstride=1, cmap=cm.gnuplot,
-        linewidth=0, antialiased=False)
+        # axis labels and limits
+        ax.set_xlim(-Nl[0]/2,Nl[0]/2-1)
+        ax.set_ylim(-Nl[1]/2,Nl[1]/2-1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
 
+        # caution : RectBivariateSpline flips x & y axes, so must enter arguments as (y,x), not (x,y)
 
-#,levels=np.arange(-0.0001,0.48,0.00005)
-fig.colorbar(surf,shrink=0.75,pad=0.01)
-"""
+        #surf = ax.plot_surface(xi, yi, plot_func(yi,xi), rstride=1, cstride=1, cmap=cm.gnuplot,
+        #                       linewidth=0, antialiased=False)
+
+        if (('psi_r' not in plot_flag) or ('tot' in plot_flag)):
+            surf = ax.plot_surface(xi, yi, plot_func(yi,xi), rstride=1, cstride=1, cmap=cm.gnuplot,
+                               linewidth=0, antialiased=False)
+        else:
+            surf = ax.plot_surface(xi, yi, np.sqrt(plot_func_re(yi,xi)**2+plot_func_im(yi,xi)**2), rstride=1, cstride=1, cmap=cm.gnuplot,
+                               linewidth=0, antialiased=False)
+            #ax.scatter(psi_x, psi_y, mod_dd, s=20, c='cyan')
+
+        fig.colorbar(surf,shrink=0.75,pad=0.01)
+
 plt.subplots_adjust(bottom=0.15)
 
-show()
-#fig.savefig('check_new_script.pdf')
-#fig.savefig('psi_r_triplet_Lx6_Ly24_N144_l0.5_U-2.5.pdf',bbox_inches='tight')
+save=0
+for arg in sys.argv:
+    if ('save' in str(arg)) or ('Save' in str(arg)):
+        save=1
+        break
 
+if save==1:
+    if 'corr_flag' in locals():
+        if 'open_flg' in locals():
+            plotname=str(corr_flag)+'_Lx'+str(Nl[0])+'_Ly'+str(Nl[1])+'_Np'+str(Np)+'_U'+str(Uhubb)+'_l'+str(lbda)+'_'+str(dim_flag)+'_'+str(open_flg)+'.pdf'
+        else:
+            plotname=str(corr_flag)+'_Lx'+str(Nl[0])+'_Ly'+str(Nl[1])+'_Np'+str(Np)+'_U'+str(Uhubb)+'_l'+str(lbda)+'_'+str(dim_flag)+'.pdf'
+        print plotname
+        fig.savefig(plotname)
+    else:
+        if 'open_flg' in locals():
+            plotname=str(plot_flag)+'_Lx'+str(Nl[0])+'_Ly'+str(Nl[1])+'_Np'+str(Np)+'_U'+str(Uhubb)+'_l'+str(lbda)+'_'+str(dim_flag)+'_'+str(open_flg)+'.pdf'
+        else:
+            plotname=str(plot_flag)+'_Lx'+str(Nl[0])+'_Ly'+str(Nl[1])+'_Np'+str(Np)+'_U'+str(Uhubb)+'_l'+str(lbda)+'_'+str(dim_flag)+'.pdf'
+        print plotname
+        fig.savefig(plotname)
+else:
+    show()
+        
